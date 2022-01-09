a2bc05e00b0568138d68af4d50b2fdb64bd7c8c2
maestrogamboa <gamboaedgard16@gmail.com>
2022-01-09 16:02:19 -0600

    Changes1

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -3,14 +3,27 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
+<<<<<<< HEAD
+=======
+from utilities.customLogger import LogGen
+>>>>>>> d523123 (Changes1)
 
 class Test_001_Login:
     baseURL = ReadConfig.getApplicationURL()
     userName = ReadConfig.getUsername()
     password = ReadConfig.getPassword()
+<<<<<<< HEAD
 
 
     def test_login(self, setup):
+=======
+    logger = LogGen.loggen()
+
+
+    def test_login(self, setup):
+        self.logger.info("*******Test_001_Login*******")
+        self.logger.info("*******Verifying Login*******")
+>>>>>>> d523123 (Changes1)
         self.driver = setup
         self.driver.get(self.baseURL)
         self.lp = LoginPage(self.driver)
@@ -20,4 +33,8 @@ class Test_001_Login:
         self.lp.clickLogin()
         self.driver.close()
         assert True
+<<<<<<< HEAD
+=======
+        self.logger.info("*******Loging test passed *******")
+>>>>>>> d523123 (Changes1)
 
