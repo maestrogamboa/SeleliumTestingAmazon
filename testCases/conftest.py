81f24fe5f4edece4e7d1f8fb76cd8b0be5fe3007
maestrogamboa <gamboaedgard16@gmail.com>
2022-01-10 13:11:21 -0600

    logs

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -6,3 +6,15 @@ import pytest
 def setup():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     return driver
+### Pytest HTMl Report ###
+
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'Amazon Test'
+    config._metadata['Module Name'] = 'Loging'
+    config._metadata['Tester'] = 'Edgard'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
